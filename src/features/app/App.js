82dc1496b8e45db8fcdae123da10568e23125388
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-01-15 23:47:23 +0530

    feat: navbar

--- src/features/app/App.js
+++ src/features/app/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react"
 
+import { NavBar } from "../navbar/navbar"
+
 import "../../styles/index.scss"
 
 export default function App() {
 
     const [theme, setTheme] = useState("dark")
 
-    return <div className={`app ${theme}_theme`}>App</div>
+    return <div className={`app ${theme}_theme`}>
+        <NavBar />
+    </div>
 }
---
+++ src/features/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./navbar.module.scss";
+
+export function NavBar() {
+    return (
+        <nav className={styles.navbar}>
+            <h3>Wordle</h3>
+        </nav>
+    );
+}
