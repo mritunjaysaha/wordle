2ad8dcc348a01339e5f33ea0cfcc251e37172e05
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-01-16 00:16:14 +0530

    feat: keyboard

--- src/features/app/App.js
+++ src/features/app/App.js
@@ -3,7 +3,7 @@ import React, { useState } from "react"
 import { NavBar } from "../navbar/navbar"
 
 import "../../styles/index.scss"
-
+import { BsBackspace } from "react-icons/bs"
 
 function Matrix({ rows, columns }) {
     const matrix = Array(rows).fill(null).map(() => Array(columns).fill(null))
@@ -20,6 +20,24 @@ function Matrix({ rows, columns }) {
     </div >
 }
 
+function Keyboard() {
+
+    const keyboardArr = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"], ["A", "S", "D", "F", "G", "H", "J", "K", "L"], ["Enter", "Z", "X", "C", "V", "B", "N", "M", <BsBackspace size={24} />]]
+
+    return <div className="keyboard_container">
+
+        <div>
+            {keyboardArr[0].map((ch, index) => <div key={index} className="keyboard_keys">{ch}</div>)}
+        </div>
+        <div>
+            {keyboardArr[1].map((ch, index) => <div key={index} className="keyboard_keys">{ch}</div>)}
+        </div>
+        <div>
+            {keyboardArr[2].map((ch, index) => <div key={index} className="keyboard_keys">{ch}</div>)}
+        </div>
+    </div>
+}
+
 export default function App() {
 
     const [theme, setTheme] = useState("dark")
@@ -27,5 +45,6 @@ export default function App() {
     return <div className={`app ${theme}_theme`}>
         <NavBar />
         <Matrix rows={6} columns={5} />
+        <Keyboard />
     </div>
 }
