7404960b2b6f3d4a52b48c477b7a31623251df39
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-01-20 00:55:40 +0530

    feat: meta tags

--- src/features/app/App.js
+++ src/features/app/App.js
@@ -6,6 +6,7 @@ import "../../styles/index.scss";
 
 import { HowToPlayModal } from "../modals/howToPlayModal";
 import { Wordle } from "../wordle/wordle";
+import { Helmet } from "react-helmet";
 
 export default function App() {
     const [theme, setTheme] = useState(
@@ -20,6 +21,25 @@ export default function App() {
 
     return (
         <div className={`app ${theme}_theme`}>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Wordle</title>
+                <meta property="og:title" content="Wordle" />
+                <meta
+                    property="og:description"
+                    content="Guess a 5 letter word in 6 tries"
+                />
+                <meta
+                    property="og:image"
+                    content="https://static.tnn.in/thumb/msid-96634355,imgsize-491555,width-450,height-250,false/96634355.jpg"
+                />
+                <meta
+                    property="og:url"
+                    content="https://wordle-eta.vercel.app/"
+                />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:card" content="Wordle | Mritunjay" />
+            </Helmet>
             <NavBar
                 theme={theme}
                 setTheme={setTheme}
